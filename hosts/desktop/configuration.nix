13b397aa220bd23dcab2699a4e9bb6efb13690a3
Martin <maragarsal2050@gmail.com>
2026-07-13 22:27:47 +0200

    🖥 Add graphical desktop stack

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -5,5 +5,6 @@
     ./hardware-configuration.nix
 
     ../../modules/system
+    ../../modules/desktop
   ];
 }
---
+++ modules/desktop/default.nix
@@ -0,0 +1,9 @@
+{ ... }:
+
+{
+  imports = [
+    ./gdm.nix
+    ./hyprland.nix
+    ./portal.nix
+  ];
+}
